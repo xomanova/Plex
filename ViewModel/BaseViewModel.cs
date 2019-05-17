2cfff70bb3a756ed7a6497d808922d975cba04b6
Meg Rehn <mrehn87@gmail.com>
2019-05-16 23:40:17 -0400

    Initial Commit

---
+++ ViewModel/BaseViewModel.cs
@@ -0,0 +1,30 @@
+﻿using PropertyChanged;
+using System.ComponentModel;
+
+namespace Plex
+{
+    /// <summary>
+    /// A base view model that fires property changed events
+    /// </summary>
+    [AddINotifyPropertyChangedInterface]
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// The event that is fired when any child property changes it's value
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => {};
+        
+        
+        /// <summary>
+        /// Call this to fire a <see cref="PropertyChanged"/>
+        /// </summary>
+        /// <param name="name"></param>
+        public void OnPropertyChange(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+
+
+
+}
